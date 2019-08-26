b38dbd60c314071d28103b148dffd4d5ae5f3d04
HongyuYang <H.Yang-10@student.tudelft.nl>
2019-08-26 03:57:15 +0200

    initial finish

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ from torch.autograd import Variable
 import clustering
 
 encode_length = 12
-batch_size = 32
+# Todo: change to 32 for the real exps
+batch_size = 8
 num_classes = 10
 learning_rate = 0.001
 epoch_lr_decrease = 30
@@ -37,7 +38,9 @@ class CNN(nn.Module):
     def forward(self, x):
         x = self.vgg.features(x)
         x = x.view(x.size(0), 512 * 7 * 7)
+        # 32 * 4096
         x_pca = self.vgg.classifier(x)
+        # 32 * 12
         x = self.fc_encode(x_pca)
         return x_pca, x
 
@@ -57,7 +60,6 @@ cnn.cuda()
 
 # Loss and Optimizer for the update CNN parameter U
 criterion = NetworkLoss()
-# Todo: make sure only requires_grad with batchU
 optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, cnn.parameters()), lr=learning_rate, momentum=0.9,
                             weight_decay=5e-4)
 
@@ -97,86 +99,71 @@ def main():
 
     # clustering algorithm to use
     deepcluster = clustering.__dict__['Kmeans'](k)
-
     # Label Y: batchY; Bit B: batchB; Center C: batchC; CNN feature U: batchU
-    # Train the Model
+    # ------------------------------------------------ Train the model ------------------------------------------------#
     for epoch in range(0, num_epochs):
         cnn.cuda().train()
         adjust_learning_rate(optimizer, epoch)
+        # ------------------------------ clustering part to get the pseudo labels--------------------------------------#
         # get the pca cnn feature from
         # 50000 * 4096
-        x_pca = compute_features(dataloader, cnn, len(dataset))
+        x_pca, x, batchU_all = compute_features(dataloader, cnn, len(dataset))
         clustering_loss = deepcluster.cluster(x_pca, verbose='store_true')
         # assign pseudo-labels
-        train_dataset = clustering.cluster_assign(deepcluster.images_lists,
-
-                                                  dataset.imgs)
-        # Todo: contains all the lablesc, first class
+        train_dataset = clustering.cluster_assign(deepcluster.images_lists, dataset.imgs)
         train_dataloader = torch.utils.data.DataLoader(
             train_dataset,
             batch_size=batch_size,
             num_workers=4,
             pin_memory=True, )
-        # Todo: add the initialization here
+        # ------------------------------Initialize the batchU, batchB, batchC -----------------------------------------#
+        # N * K
+        batchU = x
         batchB = np.sign(batchU)
-        # Todo: initialize at the begining U: 50000 * 12,
         # should be M * K
-        batchC = batchU
+        batchC = initializeC(train_dataset, batchU_all[batch_size:])
+        # ----------------------------------------- updating part -----------------------------------------------------#
         for i, (images, labels) in enumerate(train_dataloader):
-            # should be N
-            images = Variable(images.cuda())
             # should be M * N
-            # Todo: change to M * N
             labels = Variable(labels.cuda())
-
-            # initialize B, C ,U
-            # should be K * N
-            image_var = torch.autograd.Variable(images.cuda(), volatile=True)
-            batchU =cnn(image_var)[1].cpu().detach().numpy()
-            # Todo: replace with numpy here for now, might be better methods
-            batchB = np.sign(batchU)
-            # Todo: initialize at the begining U: 50000 * 12,
-            # should be M * K
-            batchC =batchU
-
-            # Fix B,U, update C
+            # --------------------------------------------  Fix B,U, updating C ---------------------------------------#
             faieal = 0.1
             labels_var = torch.autograd.Variable(labels.cuda(), volatile=True)
-            # Todo: change it to M * N
-            batchY = labels_var.cpu().numpy()
+            batchY = initializeY(labels_var.cpu().numpy())
             # should be M * K
-            Q = batchY * np.transpose(batchB)
+            Q = np.dot(batchY, batchB)
             # should be (M-1) * 1
             onesm = np.ones(batchY.shape[0] - 1)
 
             # Todo: change to 10 when the process finish
-            # different as matlab : start at 0
             for time in range(2):
                 z0 = batchC
                 # M, for cifar10 is 10
-                for m in range(batchY.size()[0]):
+                for m in range(batchY.shape[0]):
                     batchCk = np.delete(batchC, m, axis=0)
                     vkk = batchY[m]
                     batchYk = np.delete(batchY, m, axis=0)
                     # should be K * 1 inside this formula
                     batchC[m] = np.transpose(
-                        np.sign(np.transpose(Q[m]) - np.transpose(batchCk) * batchYk * np.transpose(
-                            vkk) - faieal * np.transpose(
-                            batchCk) * onesm))
+                        np.sign(np.transpose(Q[m]) - np.dot(np.dot(np.transpose(batchCk), batchYk),
+                                                            np.transpose(vkk)) - np.dot(faieal * np.transpose(
+                            batchCk), onesm)))
                 if np.linalg.norm(batchC - z0, 'fro') < 1e-6 * np.linalg.norm(z0, 'fro'):
                     break
-
+            # --------------------------------------------  Fix C,U, updating B ---------------------------------------#
             # Fix C,U update B
             muule = 0.1
-            batchB = np.sign(muule * np.transpose(batchC) * batchY + batchU)
+            batchB = np.sign(np.transpose(muule * np.dot(np.transpose(batchC), batchY)) + batchU)
 
-            # Fix B, C. update U using SGD
+            # --------------------------------------------  updating U ------------------------------------------------#
             optimizer.zero_grad()
             # change back to pytorch
             batchU = torch.from_numpy(batchU).cuda()
+            # Todo: make sure here, the second round batchU is tensor and with grad, then how to calculate batchB ?
+            batchU = torch.tensor(batchU, requires_grad=True)
             batchB = torch.from_numpy(batchB).cuda()
-            net_loss = criterion(batchU, batchB)
-            # Todo: batchU.require_grads if it's true, we need grad. bacthU true, batchB false
+            batchB = torch.tensor(batchB, requires_grad = False)
+            net_loss = criterion(batchU.float(), batchB.float())
             net_loss.backward()
             optimizer.step()
 
@@ -184,17 +171,21 @@ def main():
                 print('Epoch [%d/%d], Iter [%d/%d] Loss: %.4f '
                       % (epoch + 1, num_epochs, i + 1, len(train_dataset) // batch_size,
                          net_loss))
-
-    # Save the Trained Model
+    # --------------------------------------------  Save the Trained Model---------------------------------------------#
     torch.save(cnn.state_dict(), 'cifar.pkl')
 
 
 def compute_features(dataloader, model, N):
     # discard the label information in the dataloader
-    for i, (input_tensor, lable) in enumerate(dataloader):
-        labels = Variable(lable.cuda())
+    features_u = 0
+    features = 0
+    batchU_all = np.zeros((batch_size, encode_length))
+    for i, (input_tensor, _) in enumerate(dataloader):
         input_var = torch.autograd.Variable(input_tensor.cuda(), volatile=True)
-        aux = model(input_var)[0].cpu().numpy()
+        x_pca, x = model(input_var)
+        aux = x_pca.cpu().numpy()
+        features_u = x.detach().cpu().numpy()
+        batchU_all = np.concatenate((batchU_all, features_u), axis=0)
         if i == 0:
             features = np.zeros((N, aux.shape[1])).astype('float32')
 
@@ -203,7 +194,27 @@ def compute_features(dataloader, model, N):
         else:
             # special treatment for final batch
             features[i * batch_size:] = aux.astype('float32')
-    return features
+    return features, features_u, batchU_all
+
+
+def initializeC(train_dataset, batchU_all):
+    # Todo: make sure that this is correct regarding the label re-assignment part
+    classes = []
+    count = 0
+    for j in range(10):
+        for i in range(len(batchU_all)):
+            if train_dataset.imgs[i][1] == j:
+                count += 1
+        classes.append(np.mean(batchU_all[:count], axis=0))
+
+    return np.asarray(classes)
+
+
+def initializeY(labels):
+    batchY = np.zeros((num_classes, batch_size))
+    for i in range(batch_size):
+        batchY[labels[i]] = 1
+    return batchY
 
 
 if __name__ == '__main__':
